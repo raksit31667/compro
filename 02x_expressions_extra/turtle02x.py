407f9ea6b079c23ea2cb468ced74d543b82d6271
raksit <rak-sit@hotmail.com>
2018-03-14 17:38:38 +0700

    added comments

--- 02x_expressions_extra/turtle02x.py
+++ 02x_expressions_extra/turtle02x.py
@@ -1,3 +1,11 @@
+# Help guide Turtle back home while stop to buy food at a food shop along the way. 
+# The shop will randomly appear in Quadrant I, and the home will always appear 
+# randomly north-east of the shop.
+
+# Unfortunately both home's and shop's exact locations are not known until the program starts.
+
+
+
 LAB = "turtlelab2x.py"
 import urllib.request,pathlib
 pathlib.Path(LAB).exists() or urllib.request.urlretrieve(f"https://elab.cpe.ku.ac.th/turtlelab/{LAB}",LAB)
